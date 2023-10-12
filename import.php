92a14afe2066ea895595553cade70eea72fc43bb
mdhaval7 <130239633+mdhaval7@users.noreply.github.com>
2023-10-12 12:38:45 +0530

    import file

---
+++ import.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="process.php" method="post" enctype="multipart/form-data" >
+        <label for="">select</label>
+        <input type="file" name="import" >
+        <input type="submit" value="import">
+    </form>
+</body>
+</html>
